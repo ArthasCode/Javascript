f60348d1e614f3e23e7142ae55b9f4a775e0f1cf
ArthasCode <arthas.brunnett11@gmail.com>
2025-02-05 14:51:16 -0300

    Aula 12

---
+++ aula 12/ex011.js
@@ -0,0 +1,6 @@
+var hora = new Date().getHours()
+var minuto=new Date().getMinutes()
+console.log(`Agora são ${new Date().toLocaleTimeString()}`) 
+if (new Date().toLocaleTimeString()){
+    console.log("Boa tarde")
+}
